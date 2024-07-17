8ef5ea2b11faa29157de5201fe8b9eff0888ba1d
Xu Tao <xutao@Xus-MacBook-Pro---office.local>
2024-07-17 18:06:27 -0400

    added the KL Divergence calculation

---
+++ data_distrbution_evaluation.py
@@ -0,0 +1,142 @@
+import math
+import os
+import numpy as np
+import pandas as pd
+import pyproj
+import matplotlib.pyplot as plt
+import json
+import pyproj
+from scipy.stats import gaussian_kde
+import scipy.stats as stats
+
+def kl_divergence_multivariate(data_p, data_q):
+    """
+    Compute the Kullback-Leibler (KL) divergence between two multivariate datasets
+    data_p and data_q. Assumes each row represents a sample.
+
+    Parameters:
+    data_p (numpy array): Array of samples from dataset P (each row is a sample).
+    data_q (numpy array): Array of samples from dataset Q (each row is a sample).
+
+    Returns:
+    float: KL divergence D(P || Q).
+    """
+    # Estimate marginal densities for each variable
+
+    kdes_p = [gaussian_kde(data_p[:, i]) for i in range(data_p.shape[1])]
+    kdes_q = [gaussian_kde(data_q[:, i]) for i in range(data_q.shape[1])]
+
+    # Define a function to compute KL divergence for two KDEs
+    def kl_divergence(p, q):
+        return np.sum(p* np.log(p / q))
+
+    # Evaluate KDEs at the same points
+    min_data = np.min(np.concatenate((data_p, data_q), axis=0), axis=0)
+    max_data = np.max(np.concatenate((data_p, data_q), axis=0), axis=0)
+    points = [np.linspace(min_data[i], max_data[i], 100) for i in range(data_p.shape[1])]
+
+    # Compute KL divergence D(P || Q) for each variable and sum
+    kl_div = 0.0
+    epsilon = 1e-8
+    for i in range(data_p.shape[1]):
+        pdf_p = kdes_p[i].evaluate(points[i])
+        pdf_q = kdes_q[i].evaluate(points[i])
+        pdf_p=pdf_p+epsilon
+        pdf_q=pdf_q+epsilon
+        kl_div += kl_divergence(pdf_p, pdf_q)
+
+    return kl_div
+
+def plot_KL_divergence(x, kls_JGD, kls_EXP, kls_uni):
+    plt.figure()
+    plt.rcParams.update(
+        {'font.size': 25, 'xtick.labelsize': 'x-large', 'ytick.labelsize': 'x-large', 'axes.titlesize': 'x-large'})
+    colors = ['cornflowerblue', 'goldenrod', 'forestgreen', 'firebrick', 'purple']
+    linestyles = ['dotted', 'dashed', 'dashdot', (0, (3, 5, 1, 5, 1, 5)), 'solid', '--', 'dotted']
+    markers = ["s", "^", "*", "p", "X"]
+    plt.grid(True)
+
+    plt.plot(x, kls_JGD, label='JDG', color=colors[0], marker=markers[0], linestyle= linestyles[0], linewidth=2)
+    plt.plot(x, kls_EXP, label='Exponential', color=colors[1], marker=markers[1], linestyle= linestyles[1],linewidth=2)
+    plt.plot(x, kls_uni, label='Uniform', color=colors[2], marker=markers[2], linestyle= linestyles[2],linewidth=2)
+    plt.xlabel("# of sensors")
+    plt.ylabel(" KL Divergence")
+    plt.legend(fontsize=20, loc='upper right')
+    plt.savefig('KL Divergence of different distribution.png', format="PNG",
+                bbox_inches='tight')
+    plt.close()
+
+
+#take 500 samples from the dataset DataMatrix_313.txt
+dataset=np.loadtxt("Dataset/solar_radiation_dataset/DataMatrix_313.txt")
+data_selected_rows= dataset[:500, :] #first 500 rows
+# [0, 4 , 8, 12, 17]
+data=data_selected_rows[:, [0, 4, 8, 12, 17]]
+#data=data_selected_rows
+num_samples=500
+print("shape of the real dataset:", data.shape)
+np.savetxt("Dataset/Dataset for distribution evaluation/empirical_data.txt", data, delimiter=' ')
+mean_vector=np.mean(data, axis=0)
+# print(mean_vector)
+cov=np.cov(data.T)
+lows=np.min(data,axis=0)
+highs=np.max(data, axis=0) # Upper bounds of the uniform distributions for each variable
+# print(lows)
+# print(highs)
+num_var=len(mean_vector)
+
+# print(mean_vector)
+# print(cov)
+# print(corr_matrix)
+
+#generate the theoretical distribution of JGD
+corr_matrix=np.corrcoef(data.T)
+theoretical_JGD=np.random.multivariate_normal(mean_vector, cov, num_samples)
+print("Shape of the generated exponential array:", theoretical_JGD.shape)
+np.savetxt("Dataset/Dataset for distribution evaluation/theoretical_JGD.txt", theoretical_JGD, delimiter=' ')
+
+#generate the theoretical distribution of the exponential
+theoretical_exponential=np.zeros((num_samples, num_var))
+theoretical_exponential=np.random.exponential(scale=mean_vector, size=(num_samples, num_var))
+np.savetxt("Dataset/Dataset for distribution evaluation/theoretical_exp.txt", theoretical_exponential, delimiter=' ')
+print("Shape of the generated exponential array:", theoretical_exponential.shape)
+
+#generate the theoretical distribution of the uniform
+low = 0  # Lower bound of the uniform distribution
+theoretical_uniform = np.zeros((num_samples, num_var))
+for i in range(num_var):
+    low=lows[i]
+    high = highs[i]
+    theoretical_uniform[:, i] = np.random.uniform(low, high, size=num_samples)
+np.savetxt("Dataset/Dataset for distribution evaluation/theoretical_uniform.txt", theoretical_uniform, delimiter=' ')
+# Print the shape of the generated array
+print("Shape of the generated uniform array:", theoretical_uniform.shape)
+
+#compute the KL divergence varying the number of sensors
+dict_kls={}
+kls_JGD = []
+kls_EXP=[]
+kls_uni=[]
+for i in range(2, 6, 1):
+    empirical_data=data[:, :i]
+    data_JGD=theoretical_JGD[:, :i]
+    data_exp=theoretical_exponential[:, :i]
+    data_uni=theoretical_uniform[:,:i]
+    print("Shape of the KL Empirical array:", empirical_data.shape)
+    print("Shape of the KL JGD array:", data_JGD.shape)
+    print("Shape of the KL exp array:", data_exp.shape)
+    print("Shape of the KL uniform array:", data_uni.shape)
+    kl_JGD= kl_divergence_multivariate(empirical_data, data_JGD)
+    kls_JGD.append(kl_JGD)
+    kl_EXP = kl_divergence_multivariate(empirical_data,data_exp)
+    kls_EXP.append(kl_EXP)
+    kl_uni = kl_divergence_multivariate(empirical_data, data_uni)
+    kls_uni.append(kl_uni)
+    x=np.arange(2, 6, 1)
+plot_KL_divergence(x, kls_JGD,kls_EXP, kls_uni)
+
+
+
+
+
+
--- data_preprocessing_of_solar_radiation.py
+++ data_preprocessing_of_solar_radiation.py
@@ -8,32 +8,32 @@ import json
 import pyproj
 # Coordinates for two points
 #todo
-map={}
-# Define the Mercator projection
-mercator_proj = pyproj.Proj(proj='utm',zone='10', ellps='WGS84')
-min_esting=math.inf
-min_northing=math.inf
-with open('Dataset/solar_radiation_dataset/Solar_Radiation_sensor_map', 'r') as file:
-    for line in file:
-        # Assuming each line contains valid JSON
-        values=line.split(',')
-        dict_loc={}
-        dict_loc['Location']=values[0]
-        lat=float(values[1])
-        lon=float(values[2])
-        x,y=mercator_proj(lon,lat)
-        dict_loc['Easting'] = x -  727554
-        dict_loc['Northing'] = y -4138131
-        # if x<min_esting:
-        #     min_esting=x
-        # if y<min_northing:
-        #     min_northing=y
-        map[values[0]]=dict_loc
-
-    with open('%s/map.json' %('solar_radiation_dataset'), 'w') as json_file:
-        json_file.write(json.dumps(map, indent=4))
-    print("min_easting: %f" %(min_esting))
-    print("min_northing: %f" %(min_northing))
+# map={}
+# # Define the Mercator projection
+# mercator_proj = pyproj.Proj(proj='utm',zone='10', ellps='WGS84')
+# min_esting=math.inf
+# min_northing=math.inf
+# with open('Dataset/solar_radiation_dataset/Solar_Radiatio       n_sensor_map', 'r') as file:
+#     for line in file:
+#         # Assuming each line contains valid JSON
+#         values=line.split(',')
+#         dict_loc={}
+#         dict_loc['Location']=values[0]
+#         lat=float(values[1])
+#         lon=float(values[2])
+#         x,y=mercator_proj(lon,lat)
+#         dict_loc['Easting'] = x -  727554
+#         dict_loc['Northing'] = y -4138131
+#         # if x<min_esting:
+#         #     min_esting=x
+#         # if y<min_northing:
+#         #     min_northing=y
+#         map[values[0]]=dict_loc
+#
+#     with open('%s/map.json' %('solar_radiation_dataset'), 'w') as json_file:
+#         json_file.write(json.dumps(map, indent=4))
+#     print("min_easting: %f" %(min_esting))
+#     print("min_northing: %f" %(min_northing))
 
 # # Create a 2D graph
 # plt.figure(figsize=(8, 6))
@@ -43,13 +43,28 @@ with open('Dataset/solar_radiation_dataset/Solar_Radiation_sensor_map', 'r') as 
 # plt.title('2D Graph of Google Coordinates in Meters')
 # plt.grid()
 # plt.show()
+#
+# file_path = 'Dataset/solar_radiation_dataset/DataMatrix_315.txt'
+# data_rf = pd.read_csv(file_path, sep='\t')
+# cleaned_df= data_rf.dropna(how='any')
+# cleaned_df = cleaned_df.iloc[:, :-1].reset_index(drop=True)
+# cleaned_df.to_csv(file_path,sep='\t', index=False)
 
-file_path = 'Dataset/solar_radiation_dataset/DataMatrix_319.txt'
-data_rf = pd.read_csv(file_path, sep='\t')
-cleaned_df= data_rf.dropna(how='any')
-cleaned_df = cleaned_df.iloc[:, :-1].reset_index(drop=True)
-cleaned_df.to_csv(file_path,sep='\t', index=False)
-print(cleaned_df.shape)
+#plot the cov metrix.
+data=np.loadtxt("Dataset/solar_radiation_dataset/DataMatrix_315.txt")
+#cov_matrix=np.cov(data)
+corr_matrix=np.corrcoef(data.T)
+
+np.savetxt('Dataset/solar_radiation_dataset/correlation_matrix_315.csv', corr_matrix, delimiter=',')
+#print(cov_matrix)
+print(corr_matrix)
+
+
+
+
+
+
+
 
 
 
